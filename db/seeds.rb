bf8a0922c36f51d3d41541faa3faf06644c8090b
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2023-01-15 06:20:33 -0500

    feat: Add model, controllers and design for product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+
+Product.create!(
+  title: "Camisa Kenneth Stevens",
+  description: "Camisa para hombre Edinto",
+  image_url: "product_1.jpg",
+  price: 20.00
+)
+
+Product.create!(
+  title: "Camisa Kenneth Stevens",
+  description: "Camisa para hombre Ehan",
+  image_url: "product_2.jpg",
+  price: 20.00
+)
+
+Product.create!(
+  title: "Camisa Kenneth Stevens",
+  description: "Camisa manga corta para hombre Ehan",
+  image_url: "product_3.jpg",
+  price: 20.00
+)
+
+Product.create!(
+  title: "Polo Kenneth Stevens",
+  description: "Polo Kenneth Stevens para hombre Monet La Point",
+  image_url: "product_4.jpg",
+  price: 20.00
+)
+
+
+
+
+
+
